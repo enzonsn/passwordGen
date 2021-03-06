f686aecde3ce63021586319d2e46cace8a91118e
enzonsn <enzonsperduti@gmail.com>
2021-03-06 17:21:03 -0600

    added prompt and check for password length, confirms for other password criteria, and initialized the generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var charNum = window.prompt("How many characters? (from 8-128)");
+  if(charNum===null){window.alert("You did not input a valid number! Please try again!"); return;}
+  charNum = Number(charNum);
+  if(!Number.isInteger(charNum)|| charNum<=7 || charNum>=129){window.alert("You did not input a valid number! Please try again!"); return;}
+  var charUp = window.confirm("Include UPPERCASE letters?");
+  var charLow = window.confirm("Include lowercase letters?");
+  var charNums = window.confirm("Include numbers?");
+  var charSpec = window.confirm("Include special characters?");
+
+  var password = generatePassword(charNum, charUp, charLow, charNums, charSpec);
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+function generatePassword(length, upper, lower, nums, sp){
+  
+}
+
 generateBtn.addEventListener("click", writePassword);
